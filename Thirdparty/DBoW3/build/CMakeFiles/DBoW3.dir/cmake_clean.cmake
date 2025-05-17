f1efb66b29cb13cf8a53ae58a30427d334db8f26
fthbng77 <fatihbing38@gmail.com>
2025-05-17 21:40:53 +0300

    update

---
+++ Thirdparty/DBoW3/build/CMakeFiles/DBoW3.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "/home/fatih/Ms-Deep_SLAM/Thirdparty/DBoW3/lib/libDBoW3.pdb"
+  "/home/fatih/Ms-Deep_SLAM/Thirdparty/DBoW3/lib/libDBoW3.so"
+  "CMakeFiles/DBoW3.dir/src/BowVector.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/BowVector.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/Database.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/Database.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/DescManip.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/DescManip.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/FeatureVector.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/FeatureVector.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/QueryResults.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/QueryResults.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/ScoringObject.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/ScoringObject.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/Vocabulary.cpp.o"
+  "CMakeFiles/DBoW3.dir/src/Vocabulary.cpp.o.d"
+  "CMakeFiles/DBoW3.dir/src/quicklz.c.o"
+  "CMakeFiles/DBoW3.dir/src/quicklz.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/DBoW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build.sh
@@ -0,0 +1,27 @@
+echo "Configuring and building Thirdparty/DBoW3 ..."
+
+cd Thirdparty/DBoW3
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j3
+
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+
+cd ../../../
+
+echo "Configuring and building ORB_SLAM3 ..."
+
+mkdir build
+cd build
+cmake .. \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DUSE_CUDA=ON
+make -j$(nproc)
