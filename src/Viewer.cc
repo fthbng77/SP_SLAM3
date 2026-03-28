35be55272286fa4b12744b29fd8a46215bfedc19
fthbng77 <fatihbing38@gmail.com>
2026-03-29 00:56:54 +0300

    changed window name

--- src/Viewer.cc
+++ src/Viewer.cc
@@ -172,7 +172,7 @@ void Viewer::Run()
     Ow.SetIdentity();
     pangolin::OpenGlMatrix Twwp; // Oriented with g in the z axis, but y and x from camera
     Twwp.SetIdentity();
-    cv::namedWindow("ORB-SLAM3: Current Frame");
+    cv::namedWindow("SP_SLAM3: Current Frame");
 
     bool bFollow = true;
     bool bLocalizationMode = false;
@@ -301,7 +301,7 @@ void Viewer::Run()
             toShow = im;
         }
 
-        cv::imshow("ORB-SLAM3: Current Frame",toShow);
+        cv::imshow("SP_SLAM3: Current Frame",toShow);
         cv::waitKey(mT);
 
         if(menuReset)
